ab45e163dc766d838d4fb221e00f3c93081090fd
lailamvl <laila@DESKTOP-20FR0NQ>
2024-06-30 16:51:18 -0300

    Testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,15 +4,15 @@ describe('template spec', () => {
   })
 
   it('Insere uma tarefa', () => {
-    cy.visit('http://127.0.0.1:7001'); 
+    cy.visit('http://127.0.0.1:7001');
 
     cy.get('.new-todo')
       .type('TP2 de Engenharia de Software{enter}');
 
     cy.get('.todo-list li')
-      .should('have.length', 1) 
+      .should('have.length', 1)
       .first()
-      .should('have.text', 'TP2 de Engenharia de Software'); 
+      .should('have.text', 'TP2 de Engenharia de Software');
   });
 
   it('Insere e deleta uma tarefa', () => {
@@ -30,7 +30,7 @@ describe('template spec', () => {
   });
 
   it('Filtra tarefas completas e ativas', () => {
-    cy.visit('http://127.0.0.1:7001'); 
+    cy.visit('http://127.0.0.1:7001');
 
     cy.get('.new-todo')
       .type('TP2 de ES{enter}')
@@ -55,5 +55,62 @@ describe('template spec', () => {
     cy.contains('All').click();
     cy.get('.todo-list li')
       .should('have.length', 2);
+  });
+
+  it('Verifica tarefas marcadas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Fazer prova de ES{enter}')
+      .type('Estudar para a prova de ES{enter}');
+
+    cy.get('.todo-list li')
+      .eq(1)
+      .find('.toggle')
+      .click();
+
+    cy.get('.todo-list li')
+      .eq(1)
+      .should('have.class', 'completed');
+  });
+
+  it('Verifica tarefas desmarcadas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Fazer prova de ES{enter}')
+
+    cy.get('.todo-list li')
+      .find('.toggle')
+      .click();
+
+    cy.get('.todo-list li')
+      .should('have.class', 'completed');
+
+    cy.get('.todo-list li')
+      .find('.toggle')
+      .click();
+
+    cy.get('.todo-list li')
+      .should('not.have.class', 'completed');
+  });
+
+  it.only('Verifica editar tarefas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Fazer bolo{enter}')
+
+    cy.get('.todo-list li')
+      .dblclick();
+
+    cy.get('.todo-list li .edit')
+      .clear()
+      .type('Fazer Panqueca{enter}');
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Fazer Panqueca');
   });
 });
